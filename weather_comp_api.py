76ee2b81d37e862cd6e76992e3955539e95c2b1f
perryrjohnson <37843586+perryrjohnson@users.noreply.github.com>
2019-03-18 12:59:36 -0700

    Hooked in Weather Comp API

---
+++ weather_comp_api.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+# Weather company data API credentials
+username="8da5e845-ba8b-407c-b9dd-4a96e54c3adc"
+password="nJif88mzcF"
+ 
+cities = [('Austin', 30.266926, -97.750519), 
+          ('Atlanta', 33.753746, -84.386330),
+          ('DC', 38.889484, -77.035278),
+          ('Indianapolis', 39.832081, -86.145454),
+          ('Santa Monica', 34.024212, -118.496475)]
+
+temps = []
+precip = []
+wspd = []
+location = []
+clds = []
+uv_index = []
+
+for city in cities:
+    lat = city[1]
+    lon = city[2]
+    line='https://'+username+':'+password+'@twcservice.mybluemix.net/api/weather/v1/geocode/'+str(lat)+'/'+str(lon)+'/observations.json?&units=m'
+    r=requests.get(line)
+    weather = json.loads(r.text)
+    location = np.append(location, city[0])
+    temps = np.append(temps,weather['observation']['temp'])
+    precip = np.append(precip, weather['observation']['precip_total'])
+    wspd = np.append(wspd, weather['observation']['wspd'])
+    clds = np.append(clds, weather['observation']['clds'])
+    uv_index = np.append(uv_index, weather['observation']['uv_index'])
+  
+   
+dataset = pd.DataFrame(data = [location, temps, precip, wspd, clds, uv_index]).T
+
+
+dataset = dataset.rename(columns={0: "city", 1: "temp", 2: 'precip', 3 : 'wspd', 4 : 'clds', 5: 'uv_index'})
+
+dataset['timestamp'] = dt.datetime.now()
+
+dataset.to_csv('weather.csv')
