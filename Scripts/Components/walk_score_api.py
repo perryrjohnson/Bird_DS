1228756e5cb39775615420591d112f1244a6131d
perryrjohnson <37843586+perryrjohnson@users.noreply.github.com>
2019-05-06 14:22:07 -0700

    Update walk_score_api.py

--- Scripts/Components/walk_score_api.py
+++ Scripts/Components/walk_score_api.py
@@ -2,29 +2,35 @@ import pandas as pd
 import json
 import requests
 
-df = pd.read_csv('new_states.csv', index_col=0)
+df = pd.read_csv('data.csv', index_col=0)
+
+# find walk_score and bike_score for each geolocation
 
 # set empty walk_score list
 walk_score = []
-
-apikey = '719e7a58daf25647275ff58942226b56'
-
-
-
+bike_score = []
+apikey = 'APIKEY'
 
 
 for i in range(len(df['latitude'])):
-    url = 'http://api.walkscore.com/score?format=json&lat='+str(df['latitude'][i])+'&lon='+str(df['longitude'][i])+'&wsapikey='+apikey
+    url = 'http://api.walkscore.com/score?format=json&&lat='+str(df['latitude'][i])+'&lon='+str(df['longitude'][i])+'&transit=1&bike=1&wsapikey='+walk_apikey
     r = requests.get(url)
     data = r.json()
-    walk_score.append(data['walkscore'])
+    if 'walkscore' in data:
+        walk_score.append(data['walkscore'])
+    if 'walkscore' not in data:
+        walk_score.append(0)
+    if 'bike' in data:
+        bike_score.append(data['bike']['score'])
+    if 'bike' not in data:
+        bike_score.append(0)
+
+
+    time.sleep(0.5)    
     
+df['walk_score'] = walk_score
+df['bike_score'] = bike_score
     
 
-df['walk_score'] = walk_score
 
 df.to_csv('new_walk.csv')
-
-
-
-
